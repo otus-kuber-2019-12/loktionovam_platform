bba3c683f21db5fbad6473c334875c24fa5e2b0a
Aleksandr Loktionov <loktionovam@gmail.com>
2020-01-29 02:01:27 +0300

    EX-5-K8S-NET(*): Add canary deployment via nginx ingress
    Add canary deployment configuration to
    kubernetes-networks/canary directory
    
    Add check_nginx_canary_ingress.sh script to check canary deployment
    
    Add web-python:latest docker image to simulate new version of web app

--- misc/scripts/check_nginx_canary_ingress.sh
+++ misc/scripts/check_nginx_canary_ingress.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 WEB_ADDR=$(kubectl get svc -n ingress-nginx -o jsonpath='{.items[*].status.loadBalancer.ingress[*].ip}')
-if curl -H 'Host: web' "http://${WEB_ADDR}/web/index.html" | grep "export HOSTNAME='web-v1"; then
-    echo "'Web' application (v1) is OK via nginx ingress"
+if curl -H 'Host: web' -H 'Canary: always' "http://${WEB_ADDR}/web/index.html" | grep "export HOSTNAME='web-v2"; then
+    echo "'Web' application (v2, canary) is OK via nginx ingress"
 else
     echo "'Web' application access error via nginx ingress"
 fi
